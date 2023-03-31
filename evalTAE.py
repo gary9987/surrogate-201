184730e0e67b9ba8b81e89a82c6e180b4a086167
Gary Chen <gary369880206@gmail.com>
2023-03-31 23:18:42 +0800

    plot by training data

--- evalTAE.py
+++ evalTAE.py
@@ -1,6 +1,10 @@
 import numpy as np
 import tensorflow as tf
+
+from datasets.nb201_dataset import NasBench201Dataset
 from datasets.query_nb201 import OPS_by_IDX_201
+from datasets.transformation import OnlyValidAccTransform, ReshapeYTransform
+from datasets.utils import train_valid_test_split_dataset
 from models.TransformerAE import TransformerAutoencoderNVP
 from nats_bench import create
 import matplotlib.pyplot as plt
@@ -9,6 +13,33 @@ import matplotlib.pyplot as plt
 random_seed = 0
 np.random.seed(random_seed)
 tf.random.set_seed(random_seed)
+
+
+def to_NVP_data(graph_dataset, z_dim, reg_size):
+    features = []
+    y_list = []
+    if reg_size == -1:
+        nan_size = 0
+    else:
+        nan_size = len(graph_dataset) - reg_size
+
+    to_nan_idx = np.random.choice(range(len(graph_dataset)), nan_size, replace=False)
+
+    for data in graph_dataset:
+        x = np.reshape(data.x, -1)
+        a = np.reshape(data.a, -1)
+        features.append(np.concatenate([x, a]))
+
+        #z = np.reshape(np.random.multivariate_normal([0.]*z_dim, np.eye(z_dim), 1), -1)
+        y = np.array([data.y[-1]])
+        y_list.append(y)
+
+    y_list = np.array(y_list)
+    z = np.random.multivariate_normal([0.] * z_dim, np.eye(z_dim), y_list.shape[0])
+    y_list = np.concatenate([z, y_list], axis=-1)
+    y_list[to_nan_idx, :] = np.nan
+
+    return np.array(features).astype(np.float32), np.array(y_list).astype(np.float32)
 
 
 def inverse_from_acc(model: tf.keras.Model, num_sample_z: int, z_dim: int, to_inv_acc: float):
@@ -60,11 +91,48 @@ if __name__ == '__main__':
                                       input_size=input_size, nvp_config=nvp_config)
 
     model.load_weights('modelTAE_weights')
+    datasets = train_valid_test_split_dataset(NasBench201Dataset(start=0, end=15624, hp=str(200), seed=777),
+                                              ratio=[0.9, 0.1],
+                                              shuffle=True,
+                                              shuffle_seed=0)
+
+    for key in datasets:
+        datasets[key].apply(OnlyValidAccTransform())
+
+    x_valid, y_valid = to_NVP_data(datasets['valid'], 479, -1)
 
 
     to_inv_acc = 0.95
     x = []
     y = []
+    for ly in y_valid:
+        ops_idx, adj = inverse_from_acc(model, num_sample_z=10000, z_dim=120 * d_model - 1, to_inv_acc=ly[-1])
+        ops = [OPS_by_IDX_201[i] for i in ops_idx]
+        #print(ops)
+        #print(adj)
+
+        arch_str = ops_list_to_nb201_arch_str(ops)
+        print(arch_str)
+
+        nb201api = create(None, 'tss', fast_mode=True, verbose=False)
+        idx = nb201api.query_index_by_arch(arch_str)
+
+        acc = 0
+        acc_l = [ly[-1]]
+        for seed in [777, 888]:
+            data = nb201api.get_more_info(idx, 'cifar10-valid', iepoch=199, hp='200', is_random=seed)
+            #print(data['valid-accuracy'])
+            acc += data['valid-accuracy']
+            acc_l.append(data['valid-accuracy'])
+
+            data = nb201api.get_more_info(idx, 'cifar10', iepoch=199, hp='200', is_random=seed)
+            #print(data['test-accuracy'])
+
+        acc /= 2
+        x.append(ly[-1])
+        y.append(acc)
+        print(acc_l)
+    '''
     while to_inv_acc >= 0.60:
         for i in range(20):
             ops_idx, adj = inverse_from_acc(model, num_sample_z=10000, z_dim=120 * d_model-1, to_inv_acc=to_inv_acc)
@@ -92,9 +160,9 @@ if __name__ == '__main__':
             y.append(acc)
 
         to_inv_acc -= 0.005
-
-    plt.scatter(x, y)
-    plt.xlim(0.6, 1.)
-    plt.ylim(0.6, 1.)
+    '''
+    plt.scatter(x, y, s=[1]*len(x))
+    plt.xlim(0., 100.)
+    plt.ylim(0., 100.)
     plt.show()
 
