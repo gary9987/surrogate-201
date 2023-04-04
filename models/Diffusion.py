0c56ba14e8912a5f3e7d2bad0dacc71b433a1a44
Gary Chen <gary369880206@gmail.com>
2023-04-04 23:11:17 +0800

    conditional diffusion finish

--- models/Diffusion.py
+++ models/Diffusion.py
@@ -25,7 +25,6 @@ def forward_diffusion_process(x_0, t):
     return sqrt_alphas_cumprod_t * x_0 + sqrt_oneminus_alphas_cumprod_t * noise, noise
 
 
-
 class Down(tf.keras.layers.Layer):
     def __init__(self, output_dim):
         super(Down, self).__init__()
@@ -36,7 +35,6 @@ class Down(tf.keras.layers.Layer):
     def call(self, inputs, t):
         x = self.dense(inputs)
         t_emb = self.emb_layer(t)
-        t_emb = tf.tile(t_emb, [tf.shape(x)[0], 1])
         x = x + t_emb
         return x
 
@@ -60,10 +58,13 @@ class DiffusionModel(tf.keras.Model):
         super(DiffusionModel, self).__init__()
         self.latent_dim = input_dim
         self.diffusion_steps_t = diffusion_steps_t
-        self.t_dim = diffusion_steps_t // 2 + 1
+
+        # the max diffusion_steps_t should be < t_dim / 2
+        # if diffusion_steps_t=20, then the range of diffusion_steps_t is [0, 19]
+        self.t_dim = diffusion_steps_t * 2
 
         self.down_list = [
-            Down(input_dim//2),
+            Down(input_dim // 2),
             Down(input_dim // 4),
             Down(input_dim // 6)
         ]
@@ -74,28 +75,46 @@ class DiffusionModel(tf.keras.Model):
             Up(input_dim)
         ]
 
-    def call(self, x, t):
-        t = tf.constant([t], dtype=tf.float32)
-        t = tf.expand_dims(self.pos_encoding(t, self.t_dim), axis=0)
+    def unet_forward(self, x, t):
         for i in self.down_list:
             x = i(x, t)
         for i in self.up_list:
             x = i(x, t)
         return x
 
+    def call(self, x, t):
+        t = tf.constant(t, dtype=tf.float32)
+        t = self.pos_encoding(t, self.t_dim)
+        x = self.unet_forward(x, t)
+        return x
+
     def pos_encoding(self, t, channels):
+        t = tf.reshape(t, [-1, 1])
         inv_freq = 1.0 / (
-                10000 ** (tf.range(0, channels, 2, dtype=tf.float32) / channels)
+                10000.0 ** (tf.range(0, channels, 2, dtype=tf.float32) / channels)
         )
-        pos_enc_a = tf.math.sin(tf.transpose(t) * inv_freq)
-        pos_enc_b = tf.math.cos(tf.transpose(t) * inv_freq)
+        pos_enc_a = tf.sin(t * inv_freq)
+        pos_enc_b = tf.cos(t * inv_freq)
         pos_enc = tf.concat([pos_enc_a, pos_enc_b], axis=-1)
         return pos_enc
 
 
+class ConditionalDiffusionModel(DiffusionModel):
+    def __init__(self, input_dim, diffusion_steps_t):
+        super(ConditionalDiffusionModel, self).__init__(input_dim, diffusion_steps_t)
+        self.acc_emb = tf.keras.layers.Dense(self.t_dim)
+
+    def call(self, x, acc, t):
+        t = tf.constant(t, dtype=tf.float32)
+        t = self.pos_encoding(t, self.t_dim)
+        acc = self.acc_emb(acc)
+        x = self.unet_forward(x, t + acc)
+        return x
+
+
 if __name__ == '__main__':
-    model = DiffusionModel(input_dim=20, diffusion_steps_t=20)
+    model = ConditionalDiffusionModel(input_dim=20, diffusion_steps_t=20)
     inp = tf.random.normal(shape=(2, 20))
 
-    a = model(inp, 1)
+    a = model(inp, tf.constant([[90.], [92.]]), tf.constant([[0.], [1.]]))
     print(a)
