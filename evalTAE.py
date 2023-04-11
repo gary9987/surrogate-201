be9831876b88c246867317cbc98c5e5a9dd854b3
Gary <gary369880206@gmail.com>
2023-04-11 13:50:13 +0800

    train on small training data

--- evalTAE.py
+++ evalTAE.py
@@ -10,6 +10,7 @@ from nats_bench import create
 import matplotlib.pyplot as plt
 
 import matplotlib as mpl
+
 mpl.rcParams['figure.dpi'] = 300
 
 random_seed = 0
@@ -19,19 +20,21 @@ tf.random.set_seed(random_seed)
 
 def inverse_from_acc(model: tf.keras.Model, num_sample_z: int, z_dim: int, to_inv_acc):
     batch_size = int(tf.shape(to_inv_acc)[0])
-    y = tf.repeat(to_inv_acc, num_sample_z, axis=0) # (batch_size * num_sample_z, 1)
+    y = tf.repeat(to_inv_acc, num_sample_z, axis=0)  # (batch_size * num_sample_z, 1)
 
-    z = np.random.multivariate_normal([1.] * z_dim, np.eye(z_dim), size=batch_size * num_sample_z)  # (num_sample_z, z_dim)
+    z = np.random.multivariate_normal([1.] * z_dim, np.eye(z_dim),
+                                      size=batch_size * num_sample_z)  # (num_sample_z, z_dim)
     y = np.concatenate([z, y], axis=-1).astype(np.float32)  # (num_sample_z, z_dim + 1)
 
     rev_latent = model.inverse(y)  # (num_sample_z, x_dim(input_size*d_model ))
-    _, adj, ops_cls, adj_cls = model.decode(tf.reshape(rev_latent, (tf.shape(rev_latent)[0], -1, model.d_model)))  # (num_sample_z, input_size(120))
-    ops_cls = tf.reshape(ops_cls, (batch_size, num_sample_z, -1, model.num_ops)) # (batch_size, num_sample_z, 8, 7)
+    _, adj, ops_cls, adj_cls = model.decode(
+        tf.reshape(rev_latent, (tf.shape(rev_latent)[0], -1, model.d_model)))  # (num_sample_z, input_size(120))
+    ops_cls = tf.reshape(ops_cls, (batch_size, num_sample_z, -1, model.num_ops))  # (batch_size, num_sample_z, 8, 7)
     ops_vote = tf.reduce_sum(ops_cls, axis=1).numpy()  # (batch_size, 1, 8 * 7)
 
     adj = tf.reshape(adj, (batch_size, num_sample_z, model.num_adjs))  # (batch_size, num_sample_z, 8 * 8)
     adj = tf.where(tf.reduce_mean(adj, axis=1) >= 0.5, x=1., y=0.).numpy()  # (batch_size, 8 * 8)
-    adj = np.reshape(adj, (batch_size, int(adj.shape[-1]**(1/2)), int(adj.shape[-1]**(1/2))))
+    adj = np.reshape(adj, (batch_size, int(adj.shape[-1] ** (1 / 2)), int(adj.shape[-1] ** (1 / 2))))
 
     ops_idx_list = []
     adj_list = []
@@ -58,47 +61,32 @@ if __name__ == '__main__':
         'n_hid_dim': 256,
         'name': 'NVP'
     }
+
     model = TransformerAutoencoderNVP(num_layers=num_layers, d_model=d_model, num_heads=num_heads, dff=dff,
                                       input_size=input_size, nvp_config=nvp_config,
                                       num_ops=num_ops, num_nodes=num_nodes, num_adjs=num_adjs)
 
-    model.load_weights('logs/20230408-003521/modelTAE_weights_phase2')
+    # model.load_weights('logs/phase2_model/modelTAE_weights_phase2')
+    model.load_weights('logs/20230411-110852/modelTAE_weights_phase2')
     datasets = train_valid_test_split_dataset(NasBench201Dataset(start=0, end=15624, hp=str(200), seed=777),
-                                              ratio=[0.9, 0.1],
+                                              ratio=[0.8, 0.1, 0.1],
                                               shuffle=True,
                                               shuffle_seed=0)
 
+    datasets['train'] = datasets['train'][:1000]
     for key in datasets:
         datasets[key].apply(OnlyValidAccTransform())
 
-    x_valid, y_valid = to_NVP_data(datasets['valid'], 479, -1)
-    loader = tf.data.Dataset.from_tensor_slices((x_valid, y_valid)).batch(batch_size=256)
-    '''
-    diff = 0
-    for x, y in zip(x_valid, y_valid):
-        ops_idx = []
-        for i in range(8):
-            ops_idx.append(np.argmax(x[i * 7: (i + 1) * 7], axis=-1))
-        print(ops_idx)
-
-        z = tf.random.normal(shape=[1, 479])
-        y = tf.concat([z, tf.constant([[y[-1]]])], axis=-1)
-        a = model.inverse(y)
-        de = model.decode(tf.reshape(a, (1, -1, model.d_model))).numpy().reshape(-1)
-        ops_idx = []
-        for i in range(8):
-            ops_idx.append(np.argmax(de[i * 7: (i + 1) * 7], axis=-1))
-        print(ops_idx)
-
-    print(diff)
-    '''
+    x_valid, y_valid = to_NVP_data(datasets['train'], 479, -1)
+    loader = tf.data.Dataset.from_tensor_slices((x_valid, y_valid)).batch(batch_size=64)
 
     invalid = 0
     nb201api = create(None, 'tss', fast_mode=True, verbose=False)
+    
     x = []
     y = []
     for _, label_acc in loader:
-        ops_idx_lis, adj_list = inverse_from_acc(model, num_sample_z=50, z_dim=120 * d_model - 1, to_inv_acc=label_acc[:, -1:])
+        ops_idx_lis, adj_list = inverse_from_acc(model, num_sample_z=15, z_dim=120 * d_model - 1, to_inv_acc=label_acc[:, -1:])
 
         for ops_idx, adj, query_acc in zip(ops_idx_lis, adj_list, label_acc[:, -1]):
             try:
@@ -126,42 +114,76 @@ if __name__ == '__main__':
                 print('invalid')
                 invalid += 1
 
-    '''
-    to_inv_acc = 0.95
-    while to_inv_acc >= 0.60:
-        for i in range(20):
-            ops_idx, adj = inverse_from_acc(model, num_sample_z=10000, z_dim=120 * d_model-1, to_inv_acc=to_inv_acc)
-            ops = [OPS_by_IDX_201[i] for i in ops_idx]
-            print(ops)
-            print(adj)
-
-            arch_str = ops_list_to_nb201_arch_str(ops)
-            print(arch_str)
-
-            nb201api = create(None, 'tss', fast_mode=True, verbose=True)
-            idx = nb201api.query_index_by_arch(arch_str)
-
-            acc = 0
-            for seed in [777, 888]:
-                data = nb201api.get_more_info(idx, 'cifar10-valid', iepoch=199, hp='200', is_random=seed)
-                print(data['valid-accuracy'])
-                acc += data['valid-accuracy'] / 100.
-
-                data = nb201api.get_more_info(idx, 'cifar10', iepoch=199, hp='200', is_random=seed)
-                print(data['test-accuracy'])
-
-            acc /= 2
-            x.append(to_inv_acc)
-            y.append(acc)
-
-        to_inv_acc -= 0.005
-    '''
     print('Number of invalid decode', invalid)
     fig, ax = plt.subplots()
     ax.axline((0, 0), slope=1, linewidth=0.2, color='black')
     plt.scatter(x, y, s=[1]*len(x))
     plt.xlim(0., 1.)
     plt.ylim(0., 1.)
-    plt.show()
+    plt.savefig('inverse.png')
+    plt.cla()
 
+
+    x = []
+    y = []
+    for arch, label_acc in loader:
+        ops_cls, adj_cls, kl_loss, reg, flat_encoding = model(arch)
+
+        for true_acc, query_acc in zip(label_acc[:, -1], reg[:, -1]):
+            x.append(float(query_acc))
+            y.append(float(true_acc))
+
+    print('Number of invalid decode', invalid)
+    fig, ax = plt.subplots()
+    ax.axline((0, 0), slope=1, linewidth=0.2, color='black')
+    plt.scatter(x, y, s=[1] * len(x))
+    plt.xlim(0., 1.)
+    plt.ylim(0., 1.)
+    plt.savefig('regresion.png')
+    plt.cla()
+
+
+    x = []
+    y = []
+
+    to_inv_acc = 0.95
+    while to_inv_acc >= 0.05:
+        to_inv = tf.repeat(tf.constant([[to_inv_acc]]), 20, axis=0)
+        ops_idx_lis, adj_list = inverse_from_acc(model, num_sample_z=10, z_dim=120 * d_model - 1, to_inv_acc=to_inv)
+        for ops_idx, adj, query_acc in zip(ops_idx_lis, adj_list, to_inv[:, -1]):
+            try:
+                ops = [OPS_by_IDX_201[i] for i in ops_idx]
+                print(ops)
+                print(adj)
+
+                arch_str = ops_list_to_nb201_arch_str(ops)
+                print(arch_str)
+
+                idx = nb201api.query_index_by_arch(arch_str)
+
+                acc = 0
+                for seed in [777, 888]:
+                    data = nb201api.get_more_info(idx, 'cifar10-valid', iepoch=199, hp='200', is_random=seed)
+                    print(data['valid-accuracy'])
+                    acc += data['valid-accuracy'] / 100.
+
+                    data = nb201api.get_more_info(idx, 'cifar10', iepoch=199, hp='200', is_random=seed)
+                    print(data['test-accuracy'])
+
+                acc /= 2
+                x.append(to_inv_acc)
+                y.append(acc)
+            except:
+                print('invalid')
+                invalid += 1
+
+        to_inv_acc -= 0.005
+
+    print('Number of invalid decode', invalid)
+    fig, ax = plt.subplots()
+    ax.axline((0, 0), slope=1, linewidth=0.2, color='black')
+    plt.scatter(x, y, s=[1] * len(x))
+    plt.xlim(0., 1.)
+    plt.ylim(0., 1.)
+    plt.savefig('decending.png')
 
--- trainTAE_two_phase.py
+++ trainTAE_two_phase.py
@@ -13,7 +13,6 @@ import numpy as np
 from evalTAE import inverse_from_acc
 from datasets.utils import to_NVP_data
 
-
 now_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 logdir = os.path.join("logs", now_time)
 os.makedirs(logdir, exist_ok=True)
@@ -25,16 +24,13 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-
 parser = argparse.ArgumentParser(description='train TAE')
 parser.add_argument('--train_sample_amount', type=int, default=1000, help='Number of samples to train (default: 900)')
 parser.add_argument('--valid_sample_amount', type=int, default=100, help='Number of samples to train (default: 100)')
 args = parser.parse_args()
 
-
 random_seed = 0
 tf.random.set_seed(random_seed)
-
 
 num_ops = 7
 num_nodes = 8
@@ -74,7 +70,7 @@ class Trainer1(tf.keras.Model):
             # on the GradientTape.
             ops_cls, adj_cls, kl_loss, _, _ = self.model(x_batch_train, training=True)  # Logits for this minibatch
             ops_loss, adj_loss = cal_ops_adj_loss(x_batch_train, ops_cls, adj_cls)
-            rec_loss = self.ops_weight * ops_loss + self.adj_weight * adj_loss +  self.kl_weight * kl_loss
+            rec_loss = self.ops_weight * ops_loss + self.adj_weight * adj_loss + self.kl_weight * kl_loss
 
         grads = tape.gradient(rec_loss, self.model.trainable_weights)
         self.optimizer.apply_gradients(zip(grads, self.model.trainable_weights))
@@ -86,7 +82,7 @@ class Trainer1(tf.keras.Model):
         x_batch_train, _ = data
         ops_cls, adj_cls, kl_loss, _, _ = self.model(x_batch_train, training=True)  # Logits for this minibatch
         ops_loss, adj_loss = cal_ops_adj_loss(x_batch_train, ops_cls, adj_cls)
-        rec_loss = self.ops_weight * ops_loss + self.adj_weight * adj_loss +  self.kl_weight * kl_loss
+        rec_loss = self.ops_weight * ops_loss + self.adj_weight * adj_loss + self.kl_weight * kl_loss
         return {'rec_loss': rec_loss, 'ops_loss': ops_loss, 'adj_loss': adj_loss, 'kl_loss': kl_loss}
 
 
@@ -131,14 +127,16 @@ class Trainer2(tf.keras.Model):
             # The operations that the layer applies
             # to its inputs are going to be recorded
             # on the GradientTape.
-            ops_cls, adj_cls, kl_loss, y_out, x_encoding = self.model(x_batch_train, training=True)  # Logits for this minibatch
+            ops_cls, adj_cls, kl_loss, y_out, x_encoding = self.model(x_batch_train,
+                                                                      training=True)  # Logits for this minibatch
 
             # To avoid nan loss when batch size is small
             if tf.shape(non_nan_idx)[0] != 0:
                 reg_loss = self.reg_loss_fn(tf.gather(y_batch_train[:, z_dim:], non_nan_idx),
                                             tf.gather(y_out[:, z_dim:], non_nan_idx))
                 latent_loss = self.loss_latent(tf.gather(y_short, non_nan_idx),
-                                               tf.gather(tf.concat([y_out[:, :z_dim], y_out[:, -y_dim:]], axis=-1), non_nan_idx))  # * x_batch_train.shape[0]
+                                               tf.gather(tf.concat([y_out[:, :z_dim], y_out[:, -y_dim:]], axis=-1),
+                                                         non_nan_idx))  # * x_batch_train.shape[0]
             else:
                 reg_loss = 0.
                 latent_loss = 0.
@@ -165,7 +163,8 @@ class Trainer2(tf.keras.Model):
         # To avoid nan loss when batch size is small
         if tf.shape(non_nan_idx)[0] == 0:
             if self.finetune:
-                return {'total_loss': forward_loss, 'reg_loss': reg_loss, 'latent_loss': latent_loss, 'rev_loss': 0, 'rec_loss': rec_loss}
+                return {'total_loss': forward_loss, 'reg_loss': reg_loss, 'latent_loss': latent_loss, 'rev_loss': 0,
+                        'rec_loss': rec_loss}
             return {'total_loss': forward_loss, 'reg_loss': reg_loss, 'latent_loss': latent_loss, 'rev_loss': 0}
 
         # Backward loss
@@ -201,12 +200,15 @@ class Trainer2(tf.keras.Model):
         x_batch_train, y_batch_train = data
         y = y_batch_train[:, -y_dim:]
         z = y_batch_train[:, :z_dim]
+        z_rand = tf.random.normal(shape=tf.shape(z))
         y_short = tf.concat([z, y], axis=-1)
 
-        ops_cls, adj_cls, kl_loss, y_out, x_encoding = self.model(x_batch_train, training=False)  # Logits for this minibatch
+        ops_cls, adj_cls, kl_loss, y_out, x_encoding = self.model(x_batch_train,
+                                                                  training=False)  # Logits for this minibatch
 
         reg_loss = self.reg_loss_fn(y_batch_train[:, z_dim:], y_out[:, z_dim:])
-        latent_loss = self.loss_latent(y_short, tf.concat([y_out[:, :z_dim], y_out[:, -y_dim:]], axis=-1))  # * x_batch_train.shape[0]
+        latent_loss = self.loss_latent(y_short, tf.concat([y_out[:, :z_dim], y_out[:, -y_dim:]],
+                                                          axis=-1))  # * x_batch_train.shape[0]
         x_rev = self.model.inverse(y_batch_train)
         rev_loss = self.loss_backward(x_rev, x_encoding)  # * x_batch_train.shape[0]
         if self.finetune:
@@ -224,7 +226,8 @@ class Trainer2(tf.keras.Model):
                 'rev_loss': rev_loss}
 
 
-def train(phase: int, model, loader, batch_size, train_epochs, steps_per_epoch, callbacks=None, x_dim=None, y_dim=None, z_dim=None, finetune=False):
+def train(phase: int, model, loader, batch_size, train_epochs, steps_per_epoch, callbacks=None, x_dim=None, y_dim=None,
+          z_dim=None, finetune=False):
     optimizer = tf.keras.optimizers.Adam(CustomSchedule(d_model), beta_1=0.9, beta_2=0.98, epsilon=1e-9)
     if phase == 1:
         trainer = Trainer1(model)
@@ -248,32 +251,33 @@ if __name__ == '__main__':
     label_epochs = 200
 
     train_phase = [0, 1]  # 0 not train, 1 train
-    pretrained_phase1_weight = 'logs/20230409-191344/modelTAE_weights_phase1'
-    d_model = 32
-    dropout_rate = 0.1
+    pretrained_phase1_weight = 'logs/phase1_model/modelTAE_weights_phase1'
+    repeat = 20
+    d_model = 4
+    dropout_rate = 0.0
     dff = 512
-    num_layers = 6
-    num_heads = 6
+    num_layers = 3
+    num_heads = 3
     nvp_config = {
-        'n_couple_layer': 1,
-        'n_hid_layer': 1,
-        'n_hid_dim': 64,
+        'n_couple_layer': 4,
+        'n_hid_layer': 4,
+        'n_hid_dim': 256,
         'name': 'NVP'
     }
     finetune = False
 
-    batch_size = 256
+    batch_size = 512
     train_epochs = 1000
     patience = 100
 
     # 15624
     datasets = train_valid_test_split_dataset(NasBench201Dataset(start=0, end=15624, hp=str(label_epochs), seed=777),
-                                              ratio=[0.9, 0.1],
+                                              ratio=[0.8, 0.1, 0.1],
                                               shuffle=True,
                                               shuffle_seed=0)
 
-    #datasets['train'] = datasets['train'][:args.train_sample_amount]
-    #datasets['valid'] = datasets['valid'][:args.valid_sample_amount]
+    datasets['train'] = datasets['train'][:args.train_sample_amount]
+    datasets['valid'] = datasets['valid'][:args.valid_sample_amount]
 
     for key in datasets:
         if is_only_validation_data:
@@ -281,25 +285,28 @@ if __name__ == '__main__':
         else:
             datasets[key].apply(ReshapeYTransform())
 
-    x_dim = (np.reshape(datasets['train'][0].x, -1).shape[-1] + np.reshape(datasets['train'][0].a, -1).shape[-1]) * d_model  # 120 * d_model
+    x_dim = (np.reshape(datasets['train'][0].x, -1).shape[-1] + np.reshape(datasets['train'][0].a, -1).shape[
+        -1]) * d_model  # 120 * d_model
     y_dim = 1  # 1
     z_dim = x_dim - y_dim  # 479
 
-    #x_train, y_train = to_NVP_data(datasets['train'], z_dim, args.train_sample_amount)
-    x_train, y_train = to_NVP_data(datasets['train'], z_dim, -1)
-    x_valid, y_valid = to_NVP_data(datasets['valid'], z_dim, -1)
-
+    # x_train, y_train = to_NVP_data(datasets['train'], z_dim, args.train_sample_amount)
+    x_train, y_train = to_NVP_data(datasets['train'], z_dim, -1, repeat=repeat)
+    x_valid, y_valid = to_NVP_data(datasets['valid'], z_dim, -1, repeat=repeat)
+    x_test, y_test = to_NVP_data(datasets['test'], z_dim, -1)
 
     pretrained_model = TransformerAutoencoderNVP(num_layers=num_layers, d_model=d_model, num_heads=num_heads,
-                                      dff=dff, input_size=x_train.shape[-1], num_ops=num_ops, num_nodes=num_nodes,
-                                      num_adjs=num_adjs, nvp_config=nvp_config, dropout_rate=dropout_rate)
+                                                 dff=dff, input_size=x_train.shape[-1], num_ops=num_ops,
+                                                 num_nodes=num_nodes,
+                                                 num_adjs=num_adjs, nvp_config=nvp_config, dropout_rate=dropout_rate)
     pretrained_model.build(input_shape=(1, 120))
 
     nvp_config = {
         'n_couple_layer': 4,
         'n_hid_layer': 4,
         'n_hid_dim': 256,
-        'name': 'NVP'
+        'name': 'NVP',
+        'use_bias': True
     }
 
     model = TransformerAutoencoderNVP(num_layers=num_layers, d_model=d_model, num_heads=num_heads,
@@ -308,12 +315,14 @@ if __name__ == '__main__':
     model.build(input_shape=(1, 120))
     model.summary(print_fn=logger.info)
 
-    loader = {'train': tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(1024).batch(batch_size=batch_size).repeat(),
-              'valid': tf.data.Dataset.from_tensor_slices((x_valid, y_valid)).batch(batch_size=256)}
-
+    loader = {'train': tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(4096).batch(
+        batch_size=batch_size).repeat(),
+              'valid': tf.data.Dataset.from_tensor_slices((x_valid, y_valid)).batch(batch_size=256),
+              'test': tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(batch_size=256)
+              }
 
     tensorboard_callback = tf.keras.callbacks.TensorBoard(logdir, histogram_freq=1)
-    steps_per_epoch = len(datasets['train']) // batch_size
+    steps_per_epoch = x_train.shape[0] // batch_size
 
     if train_phase[0]:
         logger.info('Train phase 1')
@@ -321,24 +330,21 @@ if __name__ == '__main__':
                      tensorboard_callback,
                      EarlyStopping(monitor='val_rec_loss', patience=20, restore_best_weights=True)]
         trainer = train(1, model, loader, batch_size, train_epochs, steps_per_epoch, callbacks)
-        results = trainer.evaluate(loader['valid'], batch_size=256)
+        results = trainer.evaluate(loader['test'], batch_size=256)
         logger.info(f'{results}')
     else:
         pretrained_model.load_weights(pretrained_phase1_weight)
         model.encoder.set_weights(pretrained_model.encoder.get_weights())
         model.decoder.set_weights(pretrained_model.decoder.get_weights())
 
-
     if train_phase[1]:
         logger.info('Train phase 2')
         callbacks = [CSVLogger(os.path.join(logdir, f"learning_curve_phase2.log")),
                      tensorboard_callback,
                      EarlyStopping(monitor='val_total_loss', patience=patience, restore_best_weights=True)]
-        train(2, model, loader, batch_size, train_epochs, steps_per_epoch, callbacks,
-              x_dim=x_dim, y_dim=y_dim, z_dim=z_dim, finetune=finetune)
+        trainer = train(2, model, loader, batch_size, train_epochs, steps_per_epoch, callbacks,
+                        x_dim=x_dim, y_dim=y_dim, z_dim=z_dim, finetune=finetune)
+        results = trainer.evaluate(loader['test'], batch_size=256)
+        logger.info(f'{results}')
     else:
         exit()
-
-
-    # For testing inverse
-    print(inverse_from_acc(model, num_sample_z=50, z_dim=z_dim, to_inv_acc=[[1.0]]))
